18638161c79b4de7d808066306383a818fa82ddf
Dzhanybek Zakiriiaev <dzhanybek.zakiriiaev@gmail.com>
2024-03-03 21:25:27 -0500

    wrote RockPaperScissors function

--- CSCN71020-Assignment3-Dzhanybek Zakiriiaev/main.c
+++ CSCN71020-Assignment3-Dzhanybek Zakiriiaev/main.c
@@ -2,8 +2,22 @@
 #include <string.h>
 
 char* RockPaperScissors(char* player1, char* player2) {
-	
-	return "a";
+	if (strcmp(player1, player2) == 0) {
+		return "Draw";
+	}
+	else if ((strcmp(player1, "rock") == 0 && strcmp(player2, "scissors") == 0) ||
+		(strcmp(player1, "scissors") == 0 && strcmp(player2, "paper") == 0) ||
+		(strcmp(player1, "paper") == 0 && strcmp(player2, "rock") == 0)) {
+		return "Player1";
+	}
+	else if ((strcmp(player2, "rock") == 0 && strcmp(player1, "scissors") == 0) ||
+		(strcmp(player2, "scissors") == 0 && strcmp(player1, "paper") == 0) ||
+		(strcmp(player2, "paper") == 0 && strcmp(player1, "rock") == 0)) {
+		return "Player2";
+	}
+	else {
+		return "Invalid";
+	}
 }
 
 int main(void) {
